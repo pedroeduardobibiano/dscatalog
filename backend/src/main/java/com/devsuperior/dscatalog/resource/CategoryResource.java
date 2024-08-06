cf16d7681bc48f7e53f8311b9e2e420a5e4efeda
Pedro Eduardo Bibiano Pereira <pedroedubibiano@gmail.com>
2024-08-06 17:01:58 -0300

    DTO

--- backend/src/main/java/com/devsuperior/dscatalog/resource/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resource/CategoryResource.java
@@ -1,28 +1,27 @@
 package com.devsuperior.dscatalog.resource;
 
-import com.devsuperior.dscatalog.entites.Category;
-import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.dto.CategoryDTO;
+import com.devsuperior.dscatalog.services.CategoryService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
-    private final CategoryRepository categoryRepository;
+    private final CategoryService categoryService;
 
-    public CategoryResource(CategoryRepository categoryRepository) {
-        this.categoryRepository = categoryRepository;
+    public CategoryResource(CategoryService categoryService) {
+        this.categoryService = categoryService;
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<Category>> findAll() {
-        List<Category> list = categoryRepository.findAll();
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        List<CategoryDTO> list = categoryService.findAll();
         return ResponseEntity.ok().body(list);
     }
 
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,8 +1,10 @@
 package com.devsuperior.dscatalog.services;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entites.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,9 +18,14 @@ public class CategoryService {
     }
 
 
-    public List<Category> findAll() {
-        return categoryRepository.findAll();
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll() {
+        List<Category> list = categoryRepository.findAll();
+        return list.stream().map(CategoryDTO::new).toList();
+
     }
 
-
 }
+
+
+
